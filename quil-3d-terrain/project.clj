c316a88c01c34e463bd37866b0ad025c6a4791f1
Mochamad Lucky Pradana <13579812+ampersanda@users.noreply.github.com>
2018-10-22 19:16:34 +0700

    Update project.clj

--- quil-3d-terrain/project.clj
+++ quil-3d-terrain/project.clj
@@ -4,4 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [quil "2.7.1"]])
+                 [quil "2.7.1"]]
+  :main terrain3d.core)
